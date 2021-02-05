7961dbaff017426e5ed72145bbcad6541c205cd9
Cuma93 <74764805+Cuma93@users.noreply.github.com>
2021-02-05 10:24:36 +0100

    fix prototipo GUI

--- prototipo GUI.py
+++ prototipo GUI.py
@@ -1,30 +1,37 @@
 # Programma personalizzato con una pagina e progress bar
 import time
-from tkinter import *
-from tkinter.ttk import *
+import tkinter as tk
+from tkinter.constants import HORIZONTAL, SOLID
+from tkinter.ttk import Progressbar
  
 # setup
-root = Tk()
+root = tk.Tk()
 root.title("Menu system")
 root.geometry("800x1500+1100+0")
  
 # create frames
-objects_frame = Frame(root)
+objects_frame = tk.Frame(root)
 objects_frame.grid(row=0, column=0)
-buttons_frame = Frame(root)
+buttons_frame = tk.Frame(root)
 buttons_frame.grid(row=1, column=0)
-message_frame = Frame(root)
+message_frame = tk.Frame(root)
 message_frame.grid(row=2, column=0)
 
-holes_number = DoubleVar()
-forza_tiraggio = DoubleVar()
-Scale(objects_frame, label="NUMERO ASOLE", from_=0, to=200, bg="white", activebackground="green", troughcolor="white", width=50, length=300, relief=SOLID, tickinterval=50, orient=HORIZONTAL, variable=holes_number).grid(row=0, column=0)
-Scale(objects_frame, label="FORZA TENSIONAMENTO (kg)", from_=0, to=200, bg="white", activebackground="green", troughcolor="white", width=50, length=300, relief=SOLID, tickinterval=50, orient=HORIZONTAL, variable=forza_tiraggio).grid(row=1, column=0)
- 
+holes_number = tk.DoubleVar()
+forza_tiraggio = tk.DoubleVar()
+tk.Scale(objects_frame, label="NUMERO ASOLE", from_=0, to=200, bg="white", activebackground="green", troughcolor="white", width=50, length=300, relief=SOLID, tickinterval=50, orient=HORIZONTAL, variable=holes_number).grid(row=0, column=0)
+tk.Scale(objects_frame, label="FORZA TENSIONAMENTO (kg)", from_=0, to=200, bg="white", activebackground="green", troughcolor="white", width=50, length=300, relief=SOLID, tickinterval=50, orient=HORIZONTAL, variable=forza_tiraggio).grid(row=1, column=0)
+
+# clear screen function
+def clear(object):
+    slaves = object.grid_slaves()
+    for x in slaves:
+        x.destroy()
+
 # different items on different menus
 def setting():
     clear(buttons_frame)
-    Button(buttons_frame, text="START", command=start, padx=98).grid(row=0, column=0)
+    tk.Button(buttons_frame, text="START", command=start, padx=98).grid(row=0, column=0)
     print(holes_number.get())
     print(forza_tiraggio.get())
 
@@ -36,7 +43,7 @@ def setting():
 def start():
     clear(objects_frame)
     clear(buttons_frame)
-    Label(message_frame, text="IN ESECUZIONE...").grid(row=0, column=0)
+    tk.Label(message_frame, text="IN ESECUZIONE...").grid(row=0, column=0)
     progress_bar = Progressbar(objects_frame, orient="horizontal", mode="determinate", maximum=100, value=0)
     progress_bar.grid(row=0, column=1)
     objects_frame.update()
@@ -53,10 +60,10 @@ def start():
         time.sleep(0.01)
 
     clear(message_frame)
-    Label(message_frame, text="MISURAZIONE TERMINATA" + "\n" + "SONO €10").grid(row=0, column=0)
+    tk.Label(message_frame, text="MISURAZIONE TERMINATA" + "\n" + "SONO €10").grid(row=0, column=0)
     # inserire il campionamento e restituire un True
 
 # Pulsanti persistenti
-Button(objects_frame, text="CONFERMA PARAMETRI", command=setting, padx=98).grid(row=2, column=0)
+tk.Button(objects_frame, text="CONFERMA PARAMETRI", command=setting, padx=98).grid(row=2, column=0)
  
-mainloop()
+tk.mainloop()
